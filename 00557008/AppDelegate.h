5f1428d4a666a8819d089c4f3c6b30a53a7fe6d7
moc25400 <user08@user08de-imac.local>
2019-03-06 10:10:41 +0800

    Initial Commit

---
+++ 00557008/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  00557008
+//
+//  Created by User08 on 2019/3/6.
+//  Copyright © 2019 Chiafishh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 00557008/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  00557008
+//
+//  Created by User08 on 2019/3/6.
+//  Copyright © 2019 Chiafishh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
